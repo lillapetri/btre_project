8c88998b8490ff6cab7e07104ba651bc20d56269
Lilla Petri <pelvaaf@gmail.com>
2020-05-04 20:54:23 +0200

    Initial commit

--- listings/models.py
+++ listings/models.py
@@ -24,6 +24,6 @@ class Listing(models.Model):
     photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
     photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d', blank=True)
     is_published = models.BooleanField(default=True)
-    list_date = models.DateTimeField(default=datetime.now, blank=True)
+    list_date = models.DateTimeField(auto_now_add=True, blank=True)
     def __str__(self):
         return self.title
